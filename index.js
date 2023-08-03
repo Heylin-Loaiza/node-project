1c1164fea9556540ace21f1831ff4b9587b81ff7
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2023-08-03 17:13:35 -0600

    update index

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const {connectToDb, getDB} = require('./dbConnection/connection');
-
 const app = express();
+const bodyParser = require('body-parser'); 
+const cardsRouter = require('./routes/cardsRouter');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use('/cards', cardsRouter);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000')
 });
-
-let db;
-
-connectToDb((error) => {
-  if(!error){
-    db = getDB();
-  }
-})
